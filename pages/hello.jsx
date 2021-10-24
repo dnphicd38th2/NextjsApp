51fd6bc7228dd0633fd5a53f4f11e34a7297cd6c
phidang <dnphicd38th2@gmail.com>
2021-10-25 01:06:54 +0700

    first commit

---
+++ pages/hello.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function hello(props) {
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  );
+}
+
+hello.propTypes = {};
+
+export default hello;
